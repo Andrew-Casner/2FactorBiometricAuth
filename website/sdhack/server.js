dc3dd6afbdc2ed2e67feaafe5fe864225e1672ef
Andrew Casner <a.casner@comcast.net>
2018-10-13 12:59:18 -0600

    Angular build

---
+++ website/sdhack/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname,'')));
+
+app.use(express.static(path.join(__dirname, 'dist')));
+app.use('/', express.static(path.join(__dirname, 'dist')));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+const port = process.env.PORT || '8080';
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port, () => console.log('Website running on localhost:8080'));
