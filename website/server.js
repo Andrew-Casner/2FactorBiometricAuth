714972b1a9fd9af91e69579a403b67176f011caf
Andrew Casner <a.casner@comcast.net>
2018-10-13 02:51:14 -0600

    now serving on 8080

---
+++ website/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+const port = process.env.PORT || '8080';
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port, () => console.log('Website running on localhost:8080'));
