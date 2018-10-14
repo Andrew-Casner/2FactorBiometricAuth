a963e2254985e4e7eaa1aebc25e7f93f72be28e0
Andrew Casner <a.casner@comcast.net>
2018-10-13 18:50:24 -0600

    Landng page component

---
+++ website/sdhack/src/app/landing-page/landing-page.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { PhotoService } from './../services/photo.service';
+
+@Component({
+  selector: 'app-landing-page',
+  templateUrl: './landing-page.component.html',
+  styleUrls: ['./landing-page.component.css'],
+  providers: [PhotoService]
+})
+export class LandingPageComponent implements OnInit {
+
+  constructor(private photoService: PhotoService) { }
+
+  photos;
+
+  ngOnInit() {
+    this.photoService.getAllPhotos().subscribe(data => this.load(data));
+  }
+
+  load(data){
+    this.photos = data.photos;
+  }
+}
