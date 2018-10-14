dc6b39971b687da73ab242e203c6789d0967cf6e
Andrew Casner <a.casner@comcast.net>
2018-10-13 18:51:33 -0600

    Small changes

--- website/server.js
+++ website/server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(path.join(__dirname,'')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'sdhack/dist/sdhack/index.html'));
 });
 
 const port = process.env.PORT || '8080';
